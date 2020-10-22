203d075676cfea2c75c0c1b8047b2429f3fc40ee
Parmjit Singh <psg@pop-os.localdomain>
2020-10-22 03:05:31 +0100

    delete functionality adding new questions to existing quiz - work on updating images, score db, share link

--- quiz/src/pages/updateQuiz/updateQuiz.page.jsx
+++ quiz/src/pages/updateQuiz/updateQuiz.page.jsx
@@ -1,11 +1,14 @@
 import React , {useEffect, useState} from 'react';
-import {useHistory} from 'react-router-dom'
-import UpdateQuestions from '../../components/updateQuestions/updateQuestions.component';
+import {useHistory} from 'react-router-dom';
+// import UpdateQuestions from '../../components/updateQuestions/updateQuestions.component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
 import './updateQuiz.style.scss';
 import axios from 'axios';
 
 const quizAPI = process.env.REACT_APP_API_QUIZ;
 const questionsAPI = process.env.REACT_APP_API_QUESTION;
+const uploadAPI = process.env.REACT_APP_API_UPLOADS
 
 const UpdateQuiz = () => {
   const history = useHistory();
@@ -13,13 +16,13 @@ const UpdateQuiz = () => {
     id: '',
     title: '',
   });
+  const [questions, setQuestions] = useState([])
+  const [question, setQuestion] = useState({})
+  const [file, setFile] = useState('');
+  const [imageState, setImageState] = useState({
+    file: null,
+  });
 
-  const [questions, setQuestions] = useState([
-  ])
-
-  const [question, setQuestion] = useState({
-    // quizID: quiz.id,
-  })
   useEffect(() => {
     async function getQuiz() {
       const param = window.location.search;
@@ -50,35 +53,90 @@ const UpdateQuiz = () => {
       const {id} = e.target
       const state = questions[id];
       setQuestion({...state})
+      setImageState({file:state.image});
+
   }
+
+  //   const handleFileUpload = async (e) => { //submitting image first then posting the data
+  //   e.preventDefault();
+  //   const formData = new FormData();
+  //   formData.append('photo', file);
+  //   let res = await axios.post(uploadAPI, formData);
+  //   setPost({...post, image:res.data.file});
+  // };
+
   
   const handleSubmit = async () => {
     await axios.patch(`${questionsAPI}/${question.id}`, question)
+    window.location.reload();
+  }
+
+  const handleDelete = (e) => {
+    const {id} = e.currentTarget;
+    axios.delete(`${questionsAPI}/${id}`);
+    window.location.reload();
   }
 
   const handleChange = (e) => {
-    const {id, value} = e.target
+    const {id, value} = e.target    
     setQuestion({...question, [id]:value})
   }
+
+    const handleFileChange = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setImageState(URL.createObjectURL(image));
+  }
+
+    const handleAddQuestion = () => {
+    const {id, title} = quiz;
+    history.push({
+          pathname:`/createQuizzes`,
+          state:{id, title},
+    })
+  };
+
   return (
     <div>
     <h2> {quiz.title} </h2>
+
+    <div className="questionOptions">
       { questions.map((element , i) => {
-      const {question} = element;
-      return (<a key={i} id={i} className='questionLink' onClick={handleClick}> {question} <br /></a>)
-    })}
+        const {question} = element;
+        return (
+          <div key={i} className="cross"> 
+          <button key={i} id={i} className='questionLink' onClick={handleClick}> 
+          {question} 
+          {/* // eslint-disable-next-line */}
+          <a className="delete" key={i} id={element.id} onClick={handleDelete}><FontAwesomeIcon key={i} className="fontIcon" id={element.id} icon={faTimes} size="2x" /></a> 
+          <br />
+          </button>
+          </div>
+          
+          )
+      })}
+    </div>
+      <button className="newQuestion" onClick={handleAddQuestion} > Add New Question </button>
       {/* <UpdateQuestions /> */}
-        <form onSubmit={(e) => e.preventDefault()} >
+      <div className="form-container">
+        <form className="changeQuestionForm" onSubmit={(e) => e.preventDefault()} encType="multipart/form-data" >
           {/*  htmlFor is to tell react what thehtml entered is going to be for */}
               <input
                 type="file"
                 name="photo"
+                onChange={handleFileChange}
               />
+              {/* <button type="submit" className="btn" onClick={handleImageUpload}> submit </button> */}
+        { imageState.file  ? imageState.file.includes("uploads") ? (
+        <img src={imageState} alt=' Placeholder ' /> ) : ( <img src={require(`../../../public/${imageState.file}`)} alt=' Placeholder '/> )  : <div /> }
               {/* <button type="submit" className="btn" onClick={handleImageUpload}> submit </button> */}
         {/* { imageState ? (
         <img src={imageState} alt=' Placeholder '/>) : <div/>} */}
+          
           <h2> Question Title </h2>
+          
           <input
+              className = "inputField"
               type="text"
               id="question"
               onChange={handleChange}
@@ -86,6 +144,7 @@ const UpdateQuiz = () => {
             />
           <h1 htmlFor="answers"> Answers </h1>
             <input
+              className = "inputField"
               type="text"
               id="answerSelectionOne"
               onChange={handleChange}
@@ -93,6 +152,7 @@ const UpdateQuiz = () => {
 
             />
             <input
+              className = "inputField"
               type="text"
               id="answerSelectionTwo"
               onChange={handleChange}
@@ -101,6 +161,7 @@ const UpdateQuiz = () => {
             />
             {' '}
             <input
+              className = "inputField"
               type="text"
               id="answerSelectionThree"
               onChange={handleChange}
@@ -109,13 +170,15 @@ const UpdateQuiz = () => {
             />
             {' '}
             <input
+              className = "inputField"
               type="text"
               id="answerSelectionFour"
               onChange={handleChange}
               value={question.answerSelectionFour || ''}
             />
-          <button type="submit" className="btn" onClick={handleSubmit}> Submit Quiz Title </button>
+          <button type="submit" className="btn" onClick={handleSubmit}> Change Question </button>
       </form>
+      </div>
     </div>
   )
 }
